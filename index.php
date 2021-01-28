78e0238545c66299dbd8daac47a076acc6537c01
Dailee Howard <Dhoward18@mail.greenriver.edu>
2021-01-28 02:28:08 -0500

    Fixed picture and stylesheet

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
+    /*
+        Names: Dailee Howard, Nematu Ayaz-akhundzada, Ruslan Bessarab
+        Date: January 22nd, 2021
+        File: index.php
+    */
 
-// Turn on error reporting
+    // Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+    //Require the autoload file
+    require_once('vendor/autoload.php');
 
-//Require the autoload file
-require_once('vendor/autoload.php');
+    //Create an instance of the Base class
+    $f3 = Base::instance();
+    $f3->set('DEBUG', 3);
 
-//Create an instance of the Base class
-$f3 = Base::instance();
-$f3->set('DEBUG', 3);
+    //Define a default route (home page)
+    $f3->route('GET /', function () {
+        $view = new Template();
+        echo $view->render('views/home.html');
+    });
 
-//Define a default route (home page)
-$f3->route('GET /', function () {
-    $view = new Template();
-    echo $view->render('views/home.html');
-});
-
-$f3->run();
+    $f3->run();
